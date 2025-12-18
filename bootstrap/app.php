7971b527a44ed66a1006a62c50e9f316206849de
CleisonFreitas <cleison51@hotmail.com>
2025-12-18 09:35:13 -0300

    feat: adding lang to messages

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Http\Middleware\EnsureClient;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
@@ -22,7 +24,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $exceptions->render(function (NotFoundHttpException $e, Request $request) {
             return response()->json([
                 'status' => Response::HTTP_NOT_FOUND,
-                'message' => 'Arquivo/rota não localizado.'
+                'message' => $e->getMessage() ?: 'Arquivo/rota não localizado.'
             ], Response::HTTP_NOT_FOUND);
         });
         $exceptions->render(function (Throwable $e, Request $request) {
@@ -30,5 +32,17 @@ return Application::configure(basePath: dirname(__DIR__))
                 'status' => $e->getCode() ?: Response::HTTP_INTERNAL_SERVER_ERROR,
                 'message' => $e->getMessage() ?: 'Erro interno do servidor.'
             ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        });
+        $exceptions->render(function (ValidationException $e, Request $request) {
+            return response()->json([
+                'status' => Response::HTTP_UNPROCESSABLE_ENTITY,
+                'errors' => $e->errors(),
+            ], Response::HTTP_UNPROCESSABLE_ENTITY);
+        });
+        $exceptions->render(function (ModelNotFoundException $e) {
+            return response()->json([
+                'status' => Response::HTTP_NOT_FOUND,
+                'message' => $e->getMessage() ?: 'Recurso não encontrado.'
+            ], Response::HTTP_NOT_FOUND);
         });
     })->create();
--- routes/app.php
+++ routes/app.php
@@ -4,14 +4,14 @@ use App\Http\Controllers\App\ClientAuthController;
 use App\Http\Middleware\EnsureClient;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('app')->group(function () {
+Route::prefix('app')->middleware('throttle:30,1')->group(function () {
     Route::controller(ClientAuthController::class)->group(function ($router) {
 
         $router->prefix('auth')->group(function ($router) {
             $router->post('login', 'login');
             $router->post('register', 'register');
 
-            $router->middleware(['auth:sanctum', /* EnsureClient::class */])->group(function ($router) {
+            $router->middleware('auth:sanctum')->group(function ($router) {
                 $router->middleware(EnsureClient::class)->group(function ($router) {
                     $router->post('logout', 'logout');
                     $router->get('me', 'me');
